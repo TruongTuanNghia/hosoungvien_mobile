26759bd4e43244b7a05be0154305ed9d6a961241
tuannghia <truongtuannghia.bc@gmail.com>
2020-02-20 14:46:49 +0700

    no message

--- ho_so_ung_vien/lib/Model_Api/Get_Login_All.dart
+++ ho_so_ung_vien/lib/Model_Api/Get_Login_All.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 //import 'package:sky_engine/_http/http.dart' as http;
 import 'dart:async';
 
-import 'Url_Api.dart';
+import '../Url_Api.dart';
 
 class Get_All_Login
 {
