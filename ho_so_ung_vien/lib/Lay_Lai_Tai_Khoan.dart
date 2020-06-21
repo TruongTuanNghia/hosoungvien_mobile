33b9338de5000fb13bc343bfa291da624ac6cfc1
tuannghia <truongtuannghia.bc@gmail.com>
2020-06-21 20:40:40 +0700

    no message

--- ho_so_ung_vien/lib/Lay_Lai_Tai_Khoan.dart
+++ ho_so_ung_vien/lib/Lay_Lai_Tai_Khoan.dart
@@ -17,13 +17,17 @@ class LayLaiTaiKhoanState extends State<LayLaiTaiKhoan>{
       decoration: InputDecoration(
         hintStyle: TextStyle(fontSize: 20),
         hintText: 'Nhap so dien thoai',
+          contentPadding: EdgeInsets.only(left: 10),
+          border: InputBorder.none,
       ),
+      keyboardType: TextInputType.phone,
     );
     final btnLayMatKhau= new RaisedButton(
         onPressed: layMatKhau,
             child: Text('Nhận mã xác thực',style: TextStyle(fontSize: 20,color: Colors.black)),
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(30))),
+      color: Colors.lightBlue,
     );
     return Scaffold(
       appBar: AppBar(
@@ -38,19 +42,20 @@ class LayLaiTaiKhoanState extends State<LayLaiTaiKhoan>{
           Row(
             children: <Widget>[
               Container(
-                margin: EdgeInsets.only(left: 27,top: 30),
+                margin: EdgeInsets.only(left: 32,top: 30),
                   child: Text(" Nhấp số điện thoại ",style: TextStyle(color:Colors.black,fontSize: 20,fontWeight: FontWeight.bold,))
               ),
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
                 decoration: BoxDecoration(
                   border: Border.all(width: 1.5),
                     borderRadius: BorderRadius.all(Radius.circular(100))
                 ),
-                margin: EdgeInsets.only(left: 30,right: 30,top: 17),
+                margin: EdgeInsets.only(left: 10,right: 10,top: 17),
                 width: 300,
                 child: txt_Nhap_So_Dien_hoai,
               )
@@ -62,7 +67,7 @@ class LayLaiTaiKhoanState extends State<LayLaiTaiKhoan>{
               Container(
                 margin: EdgeInsets.only(top: 30),
                 child: Text('(Hãy nhập số điện thoại bạn đã đăng ký)',
-                    style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold,backgroundColor: Colors.redAccent)),
+                    style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold)),
               )
             ],
           ),
@@ -70,7 +75,7 @@ class LayLaiTaiKhoanState extends State<LayLaiTaiKhoan>{
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
-                width: 300,
+                width: 250,
                 margin: EdgeInsets.only(top: 10),
                 child: btnLayMatKhau,
               )
